92b78532d4fbc88917202ea1940877bd0f0be8c0
mohamedamine-tarhouni <mohamedamine.tarhouni@ynov.com>
2022-01-23 15:16:11 +0100

    version fini

--- php_exam_old/actions/connect.php
+++
@@ -1,3 +0,0 @@
-<?php
-$mysql = new PDO('mysql:host=localhost;dbname=php_exam_db;charset=utf8;', 'root', ''); // connection to database "php_exam_db"
-// $mysqli = new mysqli("localhost", "root", "", "php_exam_db"); // connection to database "php_exam_db"
